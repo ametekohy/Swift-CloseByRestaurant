753548e887aff91e21f64e817be77d18b0d331fd
ametekohy <a.metekohy@hotmail.com>
2020-04-02 03:12:48 +0200

    Initial Commit

---
+++ CloseByRestaurant/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CloseByRestaurant
+//
+//  Created by Arthur Metekohy on 02/04/2020.
+//  Copyright © 2020 Arthur Metekohy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
